6ad5ddee6fa47599e954b54615778e1cd5c92717
Veeresh R <r.veeresh@ADKPISOFT.COM>
2021-05-20 19:34:44 +0530

    Basic spring boot application

---
+++ src/main/java/assessment/app/assessmentappbe/AssessmentappbeApplication.java
@@ -0,0 +1,13 @@
+package assessment.app.assessmentappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentappbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentappbeApplication.class, args);
+	}
+
+}
