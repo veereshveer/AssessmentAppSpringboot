ee27c8f6a4c7b06bb4222be7bc16d3416c74b48a
sachinentomo <80520688+sachinentomo@users.noreply.github.com>
2021-05-28 12:11:18 +0530

    Delete AdminLoginRepository.java

--- src/main/java/assessment/app/assessmentappbe/Dao/repository/AdminLoginRepository.java
+++
@@ -1,8 +0,0 @@
-package assessment.app.assessmentappbe.Dao.repository;
-
-import assessment.app.assessmentappbe.Dao.model.AdminLogin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminLoginRepository extends JpaRepository<AdminLogin, Long> {
-    public AdminLogin findByEmailId(String email);
-}
